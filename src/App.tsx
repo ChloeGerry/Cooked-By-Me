a3d96c380403b1925a723ca0c4c2d3ad1d1211eb
Chloé Gerry <chloe.gerry@gmail.com>
2023-07-02 01:05:20 +0200

    update the route file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes, HashRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Planner from './pages/Planner';
 import Recipe from './pages/Recipe';
@@ -7,14 +7,12 @@ import Error from './pages/Error';
 
 const App = () => {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/planner" element={<Planner />} />
-        <Route path="/recipe/:id/:name" element={<Recipe />} />
-        <Route path="/*" element={<Error />} />
-      </Routes>
-    </HashRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/planner" element={<Planner />} />
+      <Route path="/recipe/:id/:name" element={<Recipe />} />
+      <Route path="/*" element={<Error />} />
+    </Routes>
   );
 };
 
